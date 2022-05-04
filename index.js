154b475b9e28f67d37a5bdc4ce733d9eb24e50d4
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-04 22:39:44 +0600

    Connected to Database

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Listening to post', port)
 })
+
+// Connect with MongoDb database 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fq0uj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+    console.log("Connected DB")
+});
+
